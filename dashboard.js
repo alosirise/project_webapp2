3654cc0d4f8d2e2af0f873b5ebfb2c3a67f12e08
Alosirise <www.sompod@hotmail.com>
2019-05-11 21:23:37 +0700

    dashboard suscess
    แก้ แดชบอร์ดให้ตรงกับ Json

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,45 @@
 $(function () {
 
+    $.get("data/products.json", function (data) {
+        var x = 0;
+        for (var i = 0; i < data.length; i++) {
+            x = x + 1;
+        }
+        $("#dashproducts").append(x);
 
+    });
+        
+ 
+    $.get("data/customers.json", function (data) {
+        
+        var x = 0;
+        for (var i = 0; i < (data.customerJson).length; i++) {
+            x = x + 1;
+        }
+        $("#dashcustomers").append(x);
 
+    });
 
+    
+    $.get("data/suppliers.json", function (data) {
+        
+        var x = 0;
+        for (var i = 0; i < data.length; i++) {
+            x = x + 1;
+        }
+        $("#dashsuppliers").append(x);
 
+    }); 
 
+    $.get("data/orders.json", function (data) {
+        
+        var x = 0;
+        for (var i = 0; i < data.length; i++) {
+            x = x + 1;
+        }
+        $("#dashorders").append(x);
 
+    }); 
 
 
 });
